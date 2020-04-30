cedbc90ea59cf816849321540adf476ee3f298f5
Jamal <moulayjam@gmail.com>
2020-04-30 15:59:57 -0400

    Triangle Area

---
+++ AreaOfTriangle.js
@@ -0,0 +1,4 @@
+// Write a function that takes the base and height of a triangle and return its area.
+
+const triArea = (base, height) => (base * height) / 2;
+
--- SumOfTwoNumbers.js
+++ SumOfTwoNumbers.js
@@ -1,2 +1,4 @@
 // Create a function that takes two numbers as arguments and return their sum.
 
+
+
